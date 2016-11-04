f8991c2b7e948042ecbb6a073f7d59cb8e245d25
bekahlbaker <bekahlbaker@gmail.com>
2016-11-04 12:24:18 -0500

    Set up Sign In UI

---
+++ PetApp/RoundedBorderClearBackgroundButton.swift
@@ -0,0 +1,22 @@
+//
+//  RoundedBorderClearBackgroundButton.swift
+//  PetApp
+//
+//  Created by Rebekah Baker on 11/4/16.
+//  Copyright © 2016 Rebekah Baker. All rights reserved.
+//
+
+import UIKit
+
+class RoundedBorderClearBackgroundButton: UIButton {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        self.layer.cornerRadius = 3.0
+        self.layer.borderWidth = 1.0
+        self.layer.borderColor = UIColor.white.cgColor
+        
+    }
+
+}
