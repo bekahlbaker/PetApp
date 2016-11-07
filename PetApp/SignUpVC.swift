bb427e27bb26be7cce058b4d3e50c440883cd686
bekahlbaker <bekahlbaker@gmail.com>
2016-11-07 12:21:47 -0600

    fixed bug

--- PetApp/SignUpVC.swift
+++ PetApp/SignUpVC.swift
@@ -12,19 +12,19 @@ import SwiftKeychainWrapper
 
 class SignUpVC: UIViewController, UITextFieldDelegate {
     
-    @IBOutlet weak var usernameField: UITextField!
+
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
     
     @IBAction func logInPressed(_ sender: RoundedCornerButton) {
-        if let username = usernameField.text, let email = emailField.text, let password = passwordField.text {
+        if let email = emailField.text, let password = passwordField.text {
 
                     FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: { (user, error) in
                         if error != nil {
                             print("Unable to authenticate with Firebase using email - \(error)")
                         } else {
                             print("Successfully authenticated with Firebase using email")
-                            self.performSegue(withIdentifier: "toProfileVC", sender: nil)
+                            self.performSegue(withIdentifier: "toProfileVC1", sender: nil)
                             if let user = user {
                                 let userData = ["provider": user.providerID]
                                 DataService.ds.completeSignIn(id: user.uid, userData: userData)
