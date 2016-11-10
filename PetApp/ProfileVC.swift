bf3c83ed7c75f9232a004c8dee42196277a09f84
bekahlbaker <bekahlbaker@gmail.com>
2016-11-10 12:45:33 -0600

    Downloaded all user info

--- PetApp/ProfileVC.swift
+++ PetApp/ProfileVC.swift
@@ -14,7 +14,11 @@ class ProfileVC: UIViewController {
 
     @IBOutlet weak var profileImg: UIImageView!
     @IBOutlet weak var usernameLabel: UILabel!
-
+    @IBOutlet weak var fullNameLbl: UILabel!
+    @IBOutlet weak var parentsNameLbl: UILabel!
+    @IBOutlet weak var ageLbl: UILabel!
+    @IBOutlet weak var speciesLbl: UILabel!
+    @IBOutlet weak var breedLbl: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,6 +30,11 @@ class ProfileVC: UIViewController {
             if let dictionary = snapshot.value as? [String: Any] {
                 print(snapshot)
                 self.usernameLabel.text = dictionary["username"] as? String
+                self.fullNameLbl.text = dictionary["full-name"] as? String
+                self.parentsNameLbl.text = dictionary["parents-name"] as? String
+                self.ageLbl.text = dictionary["age"] as? String
+                self.speciesLbl.text = dictionary["species"] as? String
+                self.breedLbl.text = dictionary["breed"] as? String
             }
             
             
@@ -36,16 +45,14 @@ class ProfileVC: UIViewController {
         //download profile image
         
         DataService.ds.REF_CURRENT_USER.observe(.value, with: { (snapshot) in
-            
-            
-            
+
             if let dictionary = snapshot.value as? [String: Any] {
                 let url = dictionary["profileImgUrl"] as! String
                 let storage = FIRStorage.storage()
                 
                 let storageRef = storage.reference(forURL: url)
                 
-                storageRef.data(withMaxSize: 1 * 1024 * 1024) { (data, error) in
+                storageRef.data(withMaxSize: 2 * 1024 * 1024) { (data, error) in
                     if error != nil {
                         print("Unablet to download image from firebase")
                     } else {
