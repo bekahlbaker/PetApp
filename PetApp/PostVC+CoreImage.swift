bff79712bdf840b1401ba2902dd833a49759587f
bekahlbaker <bekahlbaker@gmail.com>
2016-12-01 13:04:38 -0600

    added filters

---
+++ PetApp/PostVC+CoreImage.swift
@@ -0,0 +1,15 @@
+//
+//  PostVC+CoreImage.swift
+//  PetApp
+//
+//  Created by Rebekah Baker on 12/1/16.
+//  Copyright © 2016 Rebekah Baker. All rights reserved.
+//
+
+import UIKit
+
+extension PostVC {
+    
+    
+    
+}
--- PetApp/PostVC.swift
+++ PetApp/PostVC.swift
@@ -9,19 +9,25 @@
 import UIKit
 import Firebase
 import SwiftKeychainWrapper
+import CoreImage
 
 class PostVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet weak var captionTextField: UITextView!
     
-    @IBOutlet weak var postImage: UIImageView!
+    
+    @IBOutlet weak var filterScrollView: UIScrollView!
+    
+    @IBOutlet weak var originalImage: UIImageView!
+    @IBOutlet weak var imageToFilter: UIImageView!
+    
     @IBAction func imagePickerTapped(_ sender: AnyObject) {
         present(postImagePicker, animated: true, completion: nil)
     }
     
     @IBAction func savePost(_ sender: AnyObject) {
         
-        guard let img = postImage.image, imageSelected == true else {
+        guard let img = imageToFilter.image, imageSelected == true else {
             print("Please choose an image")
             return
         }
@@ -52,6 +58,21 @@ class PostVC: UIViewController, UIImagePickerControllerDelegate, UINavigationCon
     
     var postImagePicker: UIImagePickerController!
     var imageSelected = false
+    
+    var currentImage: UIImage!
+    var context: CIContext!
+    var currentFilter: CIFilter!
+    
+    var CIFilterNames = [
+        "CIPhotoEffectChrome",
+        "CIPhotoEffectFade",
+        "CIPhotoEffectInstant",
+        "CIPhotoEffectNoir",
+        "CIPhotoEffectProcess",
+        "CIPhotoEffectTonal",
+        "CIPhotoEffectTransfer",
+        "CISepiaTone"
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -60,18 +81,129 @@ class PostVC: UIViewController, UIImagePickerControllerDelegate, UINavigationCon
         postImagePicker.delegate = self
         postImagePicker.allowsEditing = true
         
+//        context = CIContext()
+//        currentFilter = CIFilter(name: "CIPhotoEffectChrome")
+        
+//        var xCoord: CGFloat = 5
+//        let yCoord: CGFloat = 5
+//        let buttonWidth:CGFloat = 70
+//        let buttonHeight: CGFloat = 70
+//        let gapBetweenButtons: CGFloat = 5
+//        
+//        var itemCount = 0
+        
+//        for i in 0..<CIFilterNames.count {
+//            itemCount = i
+//            
+//            // Button properties
+//            let filterButton = UIButton(type: .custom)
+//            filterButton.frame = CGRect(x: xCoord, y: yCoord, width: buttonWidth, height: buttonHeight)
+//            filterButton.tag = itemCount
+//            filterButton.addTarget(self, action: #selector(PostVC.filterButtonTapped(sender:)), for: .touchUpInside)
+//            filterButton.layer.cornerRadius = 6
+//            filterButton.clipsToBounds = true
+//            
+//            // CODE FOR FILTERS WILL BE ADDED HERE...
+//            
+//            // Create filters for each button
+//            let ciContext = CIContext(options: nil)
+//            let coreImage = CIImage(image: originalImage.image!)
+//            let filter = CIFilter(name: "\(CIFilterNames[i])" )
+//            filter!.setDefaults()
+//            filter!.setValue(coreImage, forKey: kCIInputImageKey)
+//            let filteredImageData = filter!.value(forKey: kCIOutputImageKey) as! CIImage
+//            let filteredImageRef = ciContext.createCGImage(filteredImageData, from: filteredImageData.extent)
+//            let imageForButton = UIImage(cgImage: filteredImageRef!);
+//            
+//            // Assign filtered image to the button
+//            filterButton.setBackgroundImage(imageForButton, for: .normal)
+//            
+//            // Add Buttons in the Scroll View
+//            xCoord +=  buttonWidth + gapBetweenButtons
+//            filterScrollView.addSubview(filterButton)
+//        } // END FOR LOOP
+        
+        
+//        // Resize Scroll View
+//        filterScrollView.contentSize = CGSize(width: buttonWidth * CGFloat(itemCount+2), height: yCoord)
+        
+    }
+    
+    func filterButtonTapped(sender: UIButton) {
+        let button = sender as UIButton
+        
+        imageToFilter.image = button.backgroundImage(for: UIControlState.normal)
+    }
+    
+    func addFiltersToButtons() {
+        var xCoord: CGFloat = 5
+        let yCoord: CGFloat = 5
+        let buttonWidth:CGFloat = 70
+        let buttonHeight: CGFloat = 70
+        let gapBetweenButtons: CGFloat = 5
+        
+        var itemCount = 0
+        for i in 0..<CIFilterNames.count {
+            itemCount = i
+            
+            // Button properties
+            let filterButton = UIButton(type: .custom)
+            filterButton.frame = CGRect(x: xCoord, y: yCoord, width: buttonWidth, height: buttonHeight)
+            filterButton.tag = itemCount
+            filterButton.addTarget(self, action: #selector(PostVC.filterButtonTapped(sender:)), for: .touchUpInside)
+            filterButton.layer.cornerRadius = 6
+            filterButton.clipsToBounds = true
+            
+            // CODE FOR FILTERS WILL BE ADDED HERE...
+            
+            // Create filters for each button
+            let ciContext = CIContext(options: nil)
+            let coreImage = CIImage(image: originalImage.image!)
+            let filter = CIFilter(name: "\(CIFilterNames[i])" )
+            filter!.setDefaults()
+            filter!.setValue(coreImage, forKey: kCIInputImageKey)
+            let filteredImageData = filter!.value(forKey: kCIOutputImageKey) as! CIImage
+            let filteredImageRef = ciContext.createCGImage(filteredImageData, from: filteredImageData.extent)
+            let imageForButton = UIImage(cgImage: filteredImageRef!);
+            
+            // Assign filtered image to the button
+            filterButton.setBackgroundImage(imageForButton, for: .normal)
+            
+            // Add Buttons in the Scroll View
+            xCoord +=  buttonWidth + gapBetweenButtons
+            filterScrollView.addSubview(filterButton)
+        } // END FOR LOOP
+        
+        // Resize Scroll View
+        filterScrollView.contentSize = CGSize(width: buttonWidth * CGFloat(itemCount+2), height: yCoord)
     }
     
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         if let image = info[UIImagePickerControllerEditedImage] as? UIImage {
-            postImage.image = image
+            originalImage.image = image
             imageSelected = true
+//            currentImage = image
+//            
+//            let beginImage = CIImage(image: currentImage)
+//            currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+//            
+//            applyProcessing()
         } else {
             print("Valid image not selected.")
         }
         picker.dismiss(animated: true, completion: nil)
+        self.addFiltersToButtons()
     }
+    
+//    func applyProcessing() {
+////        currentFilter.setValue(intensity.value, forKey: kCIInputIntensityKey)
+//        
+//        if let cgimg = context.createCGImage(currentFilter.outputImage!, from: currentFilter.outputImage!.extent) {
+//            let processedImage = UIImage(cgImage: cgimg)
+//            postImage.image = processedImage
+//        }
+//    }
     
     func postToFirebase(imageURL: String) {
         
@@ -89,14 +221,12 @@ class PostVC: UIViewController, UIImagePickerControllerDelegate, UINavigationCon
                     "profileImgUrl": profileImgUrl as! String
                     ]
         
-//        let uid = KeychainWrapper.standard.string(forKey: KEY_UID)
-                    
         let firebasePost = DataService.ds.REF_POSTS.childByAutoId()
         firebasePost.setValue(post)
         
         self.captionTextField.text = ""
         self.imageSelected = false
-        self.postImage.image = UIImage(named: "add-image")
+        self.originalImage.image = UIImage(named: "add-image")
         
         print("POST: \(post)")
                     
