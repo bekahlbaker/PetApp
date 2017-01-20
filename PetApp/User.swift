7fcf90789d9b1e5ed10d6427eb14c10e25a92156
bekahlbaker <bekahlbaker@gmail.com>
2017-01-20 13:59:21 -0600

    Finished User Model

--- PetApp/User.swift
+++ PetApp/User.swift
@@ -13,6 +13,12 @@ class User {
     
     fileprivate var _userKey: String!
     fileprivate var _name: String!
+    fileprivate var _parentsName: String!
+    fileprivate var _age: String!
+    fileprivate var _species: String!
+    fileprivate var _breed: String!
+    fileprivate var _location: String!
+    fileprivate var _about: String!
     fileprivate var _followers: Int!
     fileprivate var _following: Int!
     
@@ -24,6 +30,30 @@ class User {
         return _name
     }
     
+    var parentsName: String {
+        return _parentsName
+    }
+    
+    var age: String {
+        return _age
+    }
+    
+    var species: String {
+        return _species
+    }
+    
+    var breed: String {
+        return _breed
+    }
+    
+    var location: String {
+        return _location
+    }
+    
+    var about: String {
+        return _about
+    }
+    
     var followers: Int {
         return _followers
     }
@@ -32,8 +62,14 @@ class User {
         return _following
     }
     
-    init(name: String, followers: Int, following: Int) {
+    init(name: String, parentsName: String, age: String, species: String, breed: String, location: String, about: String, followers: Int, following: Int) {
         self._name = name
+        self._parentsName = parentsName
+        self._age = age
+        self._species = species
+        self._breed = breed
+        self._location = location
+        self._about = about
         self._followers = followers
         self._following = following
     }
@@ -43,6 +79,24 @@ class User {
         
         if let name = userData["full-name"] as? String {
             self._name = name
+        }
+        if let parentsName = userData["parents-name"] as? String {
+            self._parentsName = parentsName
+        }
+        if let age = userData["age"] as? String {
+            self._age = age
+        }
+        if let species = userData["species"] as? String {
+            self._species = species
+        }
+        if let breed = userData["breed"] as? String {
+            self._breed = breed
+        }
+        if let location = userData["location"] as? String {
+            self._location = location
+        }
+        if let about = userData["about"] as? String {
+            self._about = about
         }
     }
     
--- PetApp/UserCell.swift
+++ PetApp/UserCell.swift
@@ -11,7 +11,16 @@ import UIKit
 class UserCell: UITableViewCell {
 
     var user: User!
+    @IBOutlet weak var coverImg: UIImageView!
+    @IBOutlet weak var profileImg: CircleImage!
     @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var parentsNameLabel: UILabel!
+    @IBOutlet weak var ageBreedSpeciesLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var aboutLabel: UILabel!
+    @IBOutlet weak var followersLabel: UILabel!
+    @IBOutlet weak var followingLabel: UILabel!
+    @IBOutlet weak var postsLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -22,5 +31,30 @@ class UserCell: UITableViewCell {
         self.user = user
         
         self.nameLabel.text = user.name
+        self.parentsNameLabel.text = user.parentsName
+        self.locationLabel.text = user.location
+        self.aboutLabel.text = user.about
+        self.followersLabel.text = String(user.followers)
+        self.followingLabel.text = String(user.following)
+        
+        if user.age != "" {
+            self.ageBreedSpeciesLabel.text = user.age
+            if user.breed != "" {
+                self.ageBreedSpeciesLabel.text = user.age + " " + user.breed
+            } else {
+                if user.species != "" {
+                    self.ageBreedSpeciesLabel.text = user.age + " " + user.species
+                }
+            }
+        } else {
+            if user.breed != "" {
+                self.ageBreedSpeciesLabel.text = user.breed
+            } else {
+                if user.species != "" {
+                    self.ageBreedSpeciesLabel.text = user.species
+                }
+            }
+        }
+        
     }
 }
